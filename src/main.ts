e7078febf828df260e5eb9404e138e0eee883fb7
KonkypenT <dimosyav10@gmail.com>
2024-12-12 14:20:53 +0700

    Описание задачи + провайдер в самом компоненте

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { FormArrayComponent } from './test-1/form-array/form-array.component';
-import { NotificationServiceInterface } from './test-2/services/notification-service.interface';
-import { ConsoleNotificationService } from './test-2/services/console-notification.service';
 import { DiComponent } from './test-2/di/di.component';
-import { PopupNotificationService } from './test-2/services/popup-notification.service';
 
 @Component({
   selector: 'app-root',
-  // template: `<app-form-array />`,
-  template: `<app-di />`,
+  template: `<app-form-array />`,
+  // template: `<app-di />`,
   imports: [FormArrayComponent, DiComponent],
-  providers: [
-    {
-      provide: NotificationServiceInterface,
-      useClass: ConsoleNotificationService, // Смена реализации здесь
-    },
-  ],
 })
 export class App {
   name = 'Angular';
--- src/test-2/di/di.component.ts
+++ src/test-2/di/di.component.ts
@@ -1,5 +1,20 @@
+// Задача: 
+// Реализовать кастомный сервис с использованием Dependency Injection
+// У тебя есть приложение, которое должно отображать уведомления разными способами 
+// (например, через консоль или всплывающее окно). Ты должен использовать Dependency Injection (DI) 
+// для подмены способа отображения уведомлений, не изменяя основной код.
+
+// Требования:
+// 1. Создай интерфейс NotificationServiceInterface с методом notify(message: string).
+// 2. Реализуй два сервиса:
+//    ConsoleNotificationService (вывод сообщений в консоль).
+//    PopupNotificationService (всплывающее окно с сообщением).
+// 3. Используй DI, чтобы можно было легко переключать между реализациями.
+// 4. Сделай так, чтобы текущая реализация выбиралась через useClass в провайдере Angular.
+
 import { Component } from '@angular/core';
 import { NotificationServiceInterface } from '../services/notification-service.interface';
+import { PopupNotificationService } from '../services/popup-notification.service';
 
 @Component({
   selector: 'app-di',
@@ -7,7 +22,13 @@ import { NotificationServiceInterface } from '../services/notification-service.i
   template: `
     <button (click)="sendNotification()">Отправить уведомление</button>
   `,
-  standalone: true 
+  standalone: true,
+  providers: [
+    {
+      provide: NotificationServiceInterface,
+      useClass: PopupNotificationService, // Смена реализации здесь
+    },
+  ],
 })
 export class DiComponent {
   constructor(private notificationService: NotificationServiceInterface) {}
